d45ff6e7425580dca21d942851b570ad984535b6
Aratrik Basak <75318952+Aratrik-02@users.noreply.github.com>
2022-01-26 11:30:09 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
