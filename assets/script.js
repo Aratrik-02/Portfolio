368cea8e5c84c1c07923111f4095972063c5e417
Aratrik Basak <75318952+Aratrik-02@users.noreply.github.com>
2022-01-27 21:05:36 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -28,6 +28,8 @@ $(document).ready(function(){
     $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
     });
 
     // toggle menu/navbar script
