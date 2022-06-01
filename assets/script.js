05b793707b63dda3725311322d9bd52f024e3955
Aratrik Basak <75318952+Aratrik-02@users.noreply.github.com>
2022-06-01 10:42:09 +0530

    Changed the owl carousel animation

--- assets/script.js
+++ assets/script.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
     });
 
     // owl carousel script
-    $('.carousel').owlCarousel({
+    /*$('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
@@ -74,5 +74,29 @@ $(document).ready(function(){
                 nav: false
             }
         }
+    });*/
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        loop:true,
+        margin:10,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },            
+            960:{
+                items:3
+            },
+        }
+    });
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
     });
 });
